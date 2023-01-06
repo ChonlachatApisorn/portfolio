a36e9c8fd05f889a0e354dd7ac45f32b95400516
ChonlachatApisorn <105207643+ChonlachatApisorn@users.noreply.github.com>
2023-01-06 11:07:23 +0700

    feat: add database (#7)
    * feat: add database
    
    * fix: format code

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get("DATABASE_URI"),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
