3933eb986424bbc3672c5b2d35a4ab20e5c6f6e8
Cholachat Apisorn <chonlachat.apisorn@gmail.com>
2023-01-06 11:05:15 +0700

    fix: format code

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-  }),
+      isGlobal: true,
+    }),
     MongooseModule.forRootAsync({
-      useFactory: async(config: ConfigService) => ({
-        uri: config.get('DATABASE_URI')
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get("DATABASE_URI"),
       }),
       inject: [ConfigService],
-    })
+    }),
   ],
 })
 export class AppModule {}
