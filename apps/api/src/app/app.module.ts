5b103ff5f13d5b55bbb2f6246979c1a4c73d9d15
Cholachat Apisorn <chonlachat.apisorn@gmail.com>
2023-01-06 14:10:12 +0700

    feat: crud user module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from "./users/user.module";
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { MongooseModule } from "@nestjs/mongoose";
       }),
       inject: [ConfigService],
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
---
+++ apps/api/src/app/users/dto/user.dto.ts
@@ -0,0 +1,21 @@
+import { MinLength } from "class-validator";
+
+export class UserDto {
+  @MinLength(4, {
+    message: "Username must be more than 4 characters",
+  })
+  username: string;
+
+  @MinLength(8, {
+    message: "Password must be more than 8 characters",
+  })
+  password: string;
+
+  bio?: string;
+
+  email?: string;
+}
+
+export class EmailVerifyDto {
+  email_verify: boolean;
+}
