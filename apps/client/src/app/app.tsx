d64a8020a592544d84dbd13b8eaee356347719c3
Cholachat Apisorn <chonlachat.apisorn@gmail.com>
2023-01-23 11:10:27 +0700

    chore: tailwind init

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,5 +1,7 @@
+import Router from "./routes";
+
 export function App() {
-  return <div>hello world</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
