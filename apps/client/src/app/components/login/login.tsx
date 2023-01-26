d48661dae209ac4f4a9423f3ba69f006e7ea38dd
Cholachat Apisorn <chonlachat.apisorn@gmail.com>
2023-01-26 11:51:25 +0700

    feat: login page and init new client app

--- apps/client/src/app/components/login/login.tsx
+++ apps/client/src/app/components/login/login.tsx
@@ -1,44 +1,93 @@
 export function Login() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
-      <form className="p-10 bg-white rounded flex justify-center items-center flex-col shadow-md">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-20 h-20 text-gray-600 mb-2"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-            clip-rule="evenodd"
-          />
-        </svg>
-        <p className="mb-5 text-3xl uppercase text-gray-600">Login</p>
-        <input
-          type="email"
-          name="email"
-          className="mb-5 p-3 w-80 focus:border-purple-700 rounded border-2 outline-none"
-          autoComplete="off"
-          placeholder="Email"
-          required
-        ></input>
-        <input
-          type="password"
-          name="password"
-          className="mb-5 p-3 w-80 focus:border-purple-700 rounded border-2 outline-none"
-          autoComplete="off"
-          placeholder="Password"
-          required
-        ></input>
-        <button
-          className="bg-purple-600 hover:bg-purple-900 text-white font-bold p-2 rounded w-80"
-          id="login"
-          type="submit"
-        >
-          <span>Login</span>
-        </button>
-      </form>
+    <div className="h-screen flex">
+      <div className="lg:flex w-full lg:w-1/2 justify-around items-center bg-[url(https://i.pinimg.com/originals/9b/51/9d/9b519dbd02b7621f4cc95e41af184c2d.jpg)]">
+        <div className="w-full mx-auto px-20 flex-col space-y-6 bg-black opacity-20 h-screen" />
+        <div className="items-center absolute pr-56">
+          <h1 className="text-white font-bold text-4xl font-sans">
+            Web Portfolio
+          </h1>
+          <p className="text-white mt-1">view detail every portfoilo</p>
+          <div className="flex justify-center lg:justify-start mt-6">
+            <a
+              href="*"
+              className="hover:bg-sky-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-sky-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2"
+            >
+              Let's Go!
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
+        <div className="w-full px-8 md:px-32 lg:px-24">
+          <form className="bg-white rounded-md shadow-2xl p-5">
+            <h1 className="text-gray-800 font-bold text-2xl mb-1">
+              Hello Again!
+            </h1>
+            <p className="text-sm font-normal text-gray-600 mb-8">
+              Welcome Back
+            </p>
+            <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                />
+              </svg>
+              <input
+                id="email"
+                className=" pl-2 w-full outline-none border-none"
+                type="email"
+                name="email"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-gray-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                className="pl-2 w-full outline-none border-none"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <button
+              type="submit"
+              className="block w-full bg-sky-600 mt-5 py-2 rounded-2xl hover:bg-sky-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
+            >
+              Login
+            </button>
+            <div className="flex justify-center mt-4">
+              <a
+                href="register"
+                className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all"
+              >
+                Don't have an account yet?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
