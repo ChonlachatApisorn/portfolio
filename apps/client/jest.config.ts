60e89a67a9689f39fd8d26c345a9498397d753ad
Cholachat Apisorn <chonlachat.apisorn@gmail.com>
2023-01-26 09:42:38 +0700

    init client

--- apps/client/jest.config.ts
+++ apps/client/jest.config.ts
@@ -1,10 +1,11 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'client',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/client',
+  coverageDirectory: '../../coverage/apps/client'
 };
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./app/app";
-import "./index.css";
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-  document.getElementById("root")
+import App from './app/app';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>
 );
