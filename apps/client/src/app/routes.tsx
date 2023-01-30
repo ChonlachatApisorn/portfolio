d6b5a42e4fa0138f7b096c7924eb1476dd852634
Cholachat Apisorn <chonlachat.apisorn@gmail.com>
2023-01-30 09:46:47 +0700

    feat: init register page

--- apps/client/src/app/routes.tsx
+++ apps/client/src/app/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/login/login";
+import Register from "./components/register/register";
 
 export function Router() {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
       <Route path="*" element={<Login />} />
     </Routes>
   );
