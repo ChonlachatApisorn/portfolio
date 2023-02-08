807e52f410a0549699113c8426b0f65a28e0149d
ChonlachatApisorn <105207643+ChonlachatApisorn@users.noreply.github.com>
2023-02-08 14:44:16 +0700

    fix: change theme to zinc color (#54)
    * fix: change theme to zinc color
    
    * fix: remove comment

--- apps/client/src/app/components/homepage/homepage.tsx
+++ apps/client/src/app/components/homepage/homepage.tsx
@@ -2,7 +2,7 @@ import UserCard from "./user-card/userCard";
 
 function HomePage() {
   return (
-    <div className="flex justify-center w-screen h-screen">
+    <div className="flex justify-center w-screen h-screen bg-zinc-700">
       <UserCard />
     </div>
   );
--- apps/client/src/app/components/navbar.tsx
+++ apps/client/src/app/components/navbar.tsx
@@ -5,14 +5,14 @@ type PropType = {
 export function Navbar({ children }: PropType) {
   return (
     <>
-      <nav className="flex items-center h-16 bg-blue-500">
+      <nav className="flex items-center h-16 bg-zinc-900">
         <div className="flex m-5 w-full justify-between">
           <div className="flex items-center">
-            <a href="*">
-              <span className="font-bold text-white text-2xl">LOGO</span>
+            <a href="homepage" className="font-bold text-white text-2xl">
+              LOGO
             </a>
           </div>
-          <div className="flex font-medium">
+          <div className="flex font-medium text-sm">
             <div className="flex items-center justify-center text-white">
               <a className="" href="*">
                 Home
@@ -21,14 +21,20 @@ export function Navbar({ children }: PropType) {
                 Profile list
               </a>
             </div>
-            <a className="mr-3" href="/register">
-              <button className="bg-white rounded-xl text-sky-500  w-20 h-10">
+            <a
+              className="mr-3 flex justify-center items-center"
+              href="/register"
+            >
+              <button className="bg-white rounded-xl text-amber-600 hover:bg-amber-600 hover:text-white hover:translate-y-[2px] transition-all w-16 h-8">
                 Sign Up
               </button>
             </a>
-            <a className="flex justify-center items-center" href="/login">
-              <div className="rounded-xl bg-white opacity-10 w-20 h-10" />
-              <button className="text-white absolute">Log in</button>
+            <a
+              className="flex justify-center items-center text-white rounded-xl hover:text-amber-400 hover:translate-y-[2px] transition-all"
+              href="/login"
+            >
+              <div className="rounded-xl bg-white opacity-10 w-16 h-8" />
+              <button className="absolute">Log in</button>
             </a>
           </div>
         </div>
